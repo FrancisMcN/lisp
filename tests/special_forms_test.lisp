e49a8e8e786d005b68f2a468ed3b85b5a5165111
Francis McNamee <francis.mcnamee@icloud.com>
2025-11-15 23:06:53 +0100

    added test

--- tests/special_forms_test.lisp
+++ tests/special_forms_test.lisp
@@ -4,12 +4,12 @@
     (let (x "hello")
         (assert (= x "hello"))))
 
-(deftest test_multiple_values_can_be_set_using_let
-    (let (a 5 b 7)
-        (assert (= (+ a b) 12))))
-
 (deftest test_shorthand_quote_special_form
     (assert (= 'x (quote x))))
 
 (deftest test_shorthand_quote_special_form2
     (assert (= 'a (quote a))))
+
+(deftest test_multiple_values_can_be_set_using_let
+    (let (a 5 b 7)
+        (assert (= (+ a b) 12))))
